fe5fc3623881acbc90d8ca1c5a73467933c6ba5e
Daniel Barenholz <hello@dbarenholz.com>
2024-10-19 17:38:33 +0200

    Use Haskeline for interactive session

--- src/Calc/Interactive.hs
+++ src/Calc/Interactive.hs
@@ -1,6 +1,26 @@
 module Calc.Interactive where
 
-import Calc.Interactive.Runner (go)
+import System.Console.Haskeline
+import Control.Monad.IO.Class (MonadIO)
 
-runInteractively :: IO ()
-runInteractively = go
+import Calc (compute)
+import Calc.Types (Options)
+
+-- | Helper method to show the result.
+showResult :: MonadIO m => Either String String -> InputT m ()
+showResult (Left err) = outputStrLn err
+showResult (Right ok) = outputStrLn $ "= " ++ ok
+
+-- | REPL using haskeline
+runInteractively :: Options -> IO ()
+runInteractively opts = runInputT defaultSettings loop
+ where
+   loop :: InputT IO ()
+   loop = do
+     minput <- getInputLine "$ "
+     case minput of
+       Nothing -> return ()
+       Just "quit" -> return ()
+       Just input  -> do
+         showResult (compute opts input)
+         loop
